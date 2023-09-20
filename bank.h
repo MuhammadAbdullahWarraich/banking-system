cf2e9cee89ee2b67f18e32c66b5585c87fd06598
Muhammad-Abdullah-Warraich <muhammadabdullah2016pk@gmail.com>
2023-09-20 11:55:27 +0500

    feat; add data members

--- bank.h
+++ bank.h
@@ -1,19 +1,30 @@
 #include "linkedlist.h"
 #include <string>
 class Bank{
-  
   class Account{
-
     class Transaction{
-      int id;
+    public:
+      int transactionID;
+      
       string type;
       float transferredamount;
       int accountnumber;
+      Transaction(int id){
+        transactionID=id;
+        
+      }
     };
-    List<Transaction> history;
-
-
+    string accountTitle;
+    int accountNumber;
+    List<Transaction> transactionHistory;
+    int accountBalance;
   };
   List<Account> accounts;
-
+  int acc_Serial;
+  int trans_Serial;
+public:
+  Bank(){
+    acc_Serial=0;
+    trans_Serial=0;
+  }
 };
