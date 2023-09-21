7ea0d3b55f390bd4deefd287e12ec0fec93162ef
Muhammad-Abdullah-Warraich <muhammadabdullah2016pk@gmail.com>
2023-09-21 09:58:25 +0500

    fix: add destructor to list class
    also indentation was fixed

--- linkedlist.h
+++ linkedlist.h
@@ -4,105 +4,107 @@ using namespace std;
 template <class T>
 class List {
 private:
-class Node {
+  class Node {
+  public:
+    T data;
+    Node* next;
+    Node* prev;
+  };
+  Node* head;
+  Node* tail;
 public:
-T data;
-Node* next;
-Node* prev;
-};
-Node* head;
-Node* tail;
-public:
-List() {
-head = new Node;
-tail = new Node;
-head->next = tail;
-tail->prev = head;
-tail->next = nullptr;
-head->prev = nullptr;
-}
+  List() {
+    head = new Node;
+    tail = new Node;
+    head->next = tail;
+    tail->prev = head;
+    tail->next = nullptr;
+    head->prev = nullptr;
+  }
 
-class Iterator {
-private:
+  class Iterator {
+  private:
 
-Node* curr;
+    Node* curr;
 
-public:
+  public:
 
-Iterator(Node* n = nullptr)
-{ curr = n; }
-Iterator(Iterator& rhs)
-{ curr = rhs.curr; }
-Iterator & operator++() {
-curr = curr->next;
-return *this;
+    Iterator(Node* n = nullptr)
+    { curr = n; }
+    Iterator(Iterator& rhs)
+    { curr = rhs.curr; }
+    Iterator & operator++() {
+      curr = curr->next;
+      return *this;
+    }
 
-}
+    Iterator& operator--() {
+      curr = curr->prev;
+      return *this;
+    }
 
-Iterator& operator--() {
-curr = curr->prev;
-return *this;
-}
+    bool operator==(const Iterator& rhs)const {
+      if (curr == rhs.curr)
+      return true;
+      else return false;
+    }
+    bool operator!=(const Iterator& rhs)const {
+      if (curr != rhs.curr)
+      return true;
+      else return false;
+    }
+    T& operator*() {
+      return curr->data;
+    }
+  };
 
-bool operator==(const Iterator& rhs)const {
+  Iterator begin() {
 
-if (curr == rhs.curr)
-return true;
-else return false;
+  Iterator itr(head->next);
+    return itr;
+  }
+  Iterator end() {
+    Iterator itr(tail);
+    return itr;
+  }
+  void print(string separator="->") {
+    Iterator itr;
+    for (itr = begin();itr != end();itr++)
+      cout << *(itr) << separator;
 
-}
-bool operator!=(const Iterator& rhs)const {
-
-if (curr != rhs.curr)
-return true;
-else return false;
-
-}
-T& operator*() {
-
-return curr->data;
-
-}
-};
-
-Iterator begin() {
-
-Iterator itr(head->next);
-return itr;
-
-}
-Iterator end() {
-
-Iterator itr(tail);
-return itr;
-
-}
-void print(string separator="->") {
-
-Iterator itr;
-for (itr = begin();itr != end();itr++)
-  cout << *(itr) << separator;
-
-cout << "\n";
-}
-Iterator insert(Iterator it, T d)
-{
-Node* p = it.curr;
-Node* t = new Node;
-t->data = d;
-t->prev = p->prev;
-t->next = p;
-p->prev->next = t;
-p->prev = t;
-Iterator ret(t);
-return ret;
-}
-void insertatHead(T d) {
-Iterator it(head->next);
-insert(it, d);
-}
-void insertatTail(T d) {
-Iterator it(tail);
-insert(it, d);
-}
+    cout << "\n";
+  }
+  Iterator insert(Iterator it, T d){
+    Node* p = it.curr;
+    Node* t = new Node;
+    t->data = d;
+    t->prev = p->prev;
+    t->next = p;
+    p->prev->next = t;
+    p->prev = t;
+    Iterator ret(t);
+    return ret;
+    }
+  void insertatHead(T d) {
+    Iterator it(head->next);
+    insert(it, d);
+  }
+  void insertatTail(T* d) {
+    Iterator it(tail);
+    insert(it, d);
+  }
+  void deleteAtStart(){
+    if (head->next!=tail){
+      Node* temp=head->next->next;
+      delete head->next;
+      head->next=temp;
+    }
+  }
+  ~List(){
+    while(head->next!=tail){
+      deleteAtStart():
+    }
+    delete head;
+    delete tail;
+  }
 };
